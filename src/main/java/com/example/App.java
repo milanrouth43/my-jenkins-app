4a9de51ad3d2ae7dccb417354e58c04036e6dda0
milanrouth43 <milanrouth43@gmail.com>
2026-01-29 12:10:07 +0530

    fist commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Milan! This is your Jenkins Build.");
+    }
+
+    public String checkStatus() {
+        return "Success";
+    }
+}
